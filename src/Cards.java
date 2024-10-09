cd3445b37c03be05afb707ed543eed1f8d0a5781
19stoyanov_j <19stoyanov_j@bexleygs.co.uk>
2024-10-09 12:05:49 +0100

    Initial commit 1.0.0

---
+++ src/Cards.java
@@ -0,0 +1,31 @@
+public class Cards {
+    private String suit;
+    private int value;
+
+    public Cards(String suit, int value) {
+        this.suit = suit;
+        this.value = value;
+    }
+    public String getSuit() {
+        return suit;
+    }
+    public String getValue() {
+        if (value == 13) {
+            return "King";
+        }
+        else if (value == 12) {
+            return "Queen";
+        }
+        else if (value == 11) {
+            return "Jack";
+        }
+        else if (value == 1) {
+            return "Ace";
+        }
+        else {
+            return Integer.toString(value);
+        }
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Cards myCard = new Cards("Spades", 11);
+        System.out.println(myCard.getValue() + "of" + myCard.getSuit());
+    }
+}
