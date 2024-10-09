2ef3089551930933ef6cd26b124844f8f35a4333
LittenSchool <19stoyanov_j@bexleygs.co.uk>
2024-10-09 13:23:12 +0100

    1.0.1.1 Test run, idk if it works but i cant be sure.

--- src/Deck.java
+++ src/Deck.java
@@ -4,17 +4,17 @@ public class Deck {
     public Deck() {
         //initialise deck
         allCards = new Cards[52];
-
+        String[] listOfSuits = {"Hearts","Spades","Clubs","Diamonds"};
         //suits
         //hearts
-
-        for (int i = 1; i <= 13; i++) {
-            Cards c = new Cards("Hearts", i);
-            allCards[i-1] = c;
+        for (int j = 0; j < 4; j++) {
+            for (int i = 1; i <= 13; i++) {
+                Cards c = new Cards(listOfSuits[j], i);
+                allCards[i - 1] = c;
+            }
         }
-        for (int i = 0; i < allCards.length; i++) {
-            allCards[i].display();
+        for (Cards allCard : allCards) {
+            allCard.display();
         }
-
-    }
+        }
 }
